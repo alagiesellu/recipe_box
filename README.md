8f120601056343479e9ee70b014101db2a26cce6
Alagie Sellu <alagiesellu@gmail.com>
2022-03-13 16:34:11 +0000

    Add laravel initial commands to start project and README.md file.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Recipe Box Deployment
+
+To deploy app;
+- Make deploy.sh file executable.
+- And then execute it.
--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+# deploy nginx server
 docker-compose up -d
-cd backend && docker-compose up -d
-cd ../
-cd frontend && docker-compose up -d
+
+# deploy laravel backer
+cd backend || exit
+docker-compose up -d
+docker-compose exec laravel composer install
+docker-compose exec laravel php artisan key:generate
+docker-compose exec laravel php artisan migrate
+docker-compose exec laravel php artisan optimize
+
+cd ../ && cd frontend || exit
+
+# deploy vue.js frontend
+docker-compose up -d
